4580f9959cd1c88e6f6b569e72a87652fea74899
Supragya Raj <supragyaraj@gmail.com>
2022-06-13 22:14:14 +0530

    refactor: DB

--- db/db.go
+++ db/db.go
@@ -99,81 +99,89 @@ func (d *DBConn) BeginTx() (context.Context, *sql.Tx) {
 	return ctx, tx
 }
 
-func (d *DBConn) AddToTx(dbCtx *context.Context, dbTx *sql.Tx, items []interface{}, bm itypes.BlockSynopsis, block uint64) {
+func (d *DBConn) AddToTx(dbCtx *context.Context, dbTx *sql.Tx, items []interface{}, bm itypes.BlockSynopsis, blockHeight uint64) {
 	currentTime := time.Now().Unix()
 	for _, item := range items {
 		query := ""
 		switch item.(type) {
 		case itypes.Mint:
-			const insquery string = "INSERT INTO %s "
-			const fields string = "(nwtype, network, 	time, 			  inserted_at, 		token0, token1, pair, amount0, amount1, amountusd, reserves0, reserves1, reservesusd, type, sender, transaction, slippage, height) "
-			const valuesfmt string = "VALUES ('%s', %d, TO_TIMESTAMP(%d), TO_TIMESTAMP(%d), '%s',   '%s',   '%s', %f,      %f,      %f,        %f,        %f,        %f,          '%s', '%s',   '%s',        %f,       %d    );"
-			item := item.(itypes.Mint)
-			query = fmt.Sprintf(insquery+fields+valuesfmt, d.dataTable, // table to insert to
-				d.Network,                      // nwtype
-				d.ChainID,                      // network
-				item.Time,                      // time
-				currentTime,                    // inserted_at
-				item.Token0.String()[2:],       // token0 (removed 0x prefix)
-				item.Token1.String()[2:],       // token1 (removed 0x prefix)
-				item.PairContract.String()[2:], // pair
-				item.Amount0,                   // amount0
-				item.Amount1,                   // amount1
-				0.0,                            // amountusd, FIXME
-				item.Reserve0,                  // reserves0
-				item.Reserve1,                  // reserves1
-				0.0,                            // reservesusd, FIXME
-				"mint",                         // type
-				"",                             // sender FIXME
-				item.Transaction.String()[2:],  // transaction (removed 0x prefix)
-				0.0,                            // slippage
-				item.Height,                    // height
-			)
+			query = d.getQueryStringMint(item.(itypes.Mint), currentTime)
 		case itypes.Burn:
-			const insquery string = "INSERT INTO %s "
-			const fields string = "(nwtype, network, 	time, 			  inserted_at, 		token0, token1, pair, amount0, amount1, amountusd, reserves0, reserves1, reservesusd, type, sender, transaction, slippage, height) "
-			const valuesfmt string = "VALUES ('%s', %d, TO_TIMESTAMP(%d), TO_TIMESTAMP(%d), '%s',   '%s',   '%s', %f,      %f,      %f,        %f,        %f,        %f,          '%s', '%s',   '%s',        %f,       %d    );"
-			item := item.(itypes.Burn)
-			query = fmt.Sprintf(insquery+fields+valuesfmt, d.dataTable, // table to insert to
-				d.Network,                      // nwtype
-				d.ChainID,                      // network
-				item.Time,                      // time
-				currentTime,                    // inserted_at
-				item.Token0.String()[2:],       // token0 (removed 0x prefix)
-				item.Token1.String()[2:],       // token1 (removed 0x prefix)
-				item.PairContract.String()[2:], // pair
-				item.Amount0,                   // amount0
-				item.Amount1,                   // amount1
-				0.0,                            // amountusd, FIXME
-				item.Reserve0,                  // reserves0
-				item.Reserve1,                  // reserves1
-				0.0,                            // reservesusd, FIXME
-				"burn",                         // type
-				"",                             // sender FIXME
-				item.Transaction.String()[2:],  // transaction (removed 0x prefix)
-				0.0,                            // slippage
-				item.Height,                    // height
-			)
-
+			query = d.getQueryStringBurn(item.(itypes.Burn), currentTime)
 		}
 		_, err := dbTx.ExecContext(*dbCtx, query)
 		util.ENOK(err)
 	}
 
 	// Add block synopsis
+	query := d.getQueryStringBlockSynopsis(blockHeight, currentTime, bm)
+	_, err := dbTx.ExecContext(*dbCtx, query)
+	util.ENOK(err)
+}
+
+func (d *DBConn) getQueryStringMint(item itypes.Mint, currentTime int64) string {
+	const insquery string = "INSERT INTO %s "
+	const fields string = "(nwtype, network, 	time, 			  inserted_at, 		token0, token1, pair, amount0, amount1, amountusd, reserves0, reserves1, reservesusd, type, sender, transaction, slippage, height) "
+	const valuesfmt string = "VALUES ('%s', %d, TO_TIMESTAMP(%d), TO_TIMESTAMP(%d), '%s',   '%s',   '%s', %f,      %f,      %f,        %f,        %f,        %f,          '%s', '%s',   '%s',        %f,       %d    );"
+	return fmt.Sprintf(insquery+fields+valuesfmt, d.dataTable, // table to insert to
+		d.Network,                      // nwtype
+		d.ChainID,                      // network
+		item.Time,                      // time
+		currentTime,                    // inserted_at
+		item.Token0.String()[2:],       // token0 (removed 0x prefix)
+		item.Token1.String()[2:],       // token1 (removed 0x prefix)
+		item.PairContract.String()[2:], // pair
+		item.Amount0,                   // amount0
+		item.Amount1,                   // amount1
+		0.0,                            // amountusd, FIXME
+		item.Reserve0,                  // reserves0
+		item.Reserve1,                  // reserves1
+		0.0,                            // reservesusd, FIXME
+		"mint",                         // type
+		"",                             // sender FIXME
+		item.Transaction.String()[2:],  // transaction (removed 0x prefix)
+		0.0,                            // slippage
+		item.Height,                    // height
+	)
+}
+
+func (d *DBConn) getQueryStringBurn(item itypes.Burn, currentTime int64) string {
+	const insquery string = "INSERT INTO %s "
+	const fields string = "(nwtype, network, 	time, 			  inserted_at, 		token0, token1, pair, amount0, amount1, amountusd, reserves0, reserves1, reservesusd, type, sender, transaction, slippage, height) "
+	const valuesfmt string = "VALUES ('%s', %d, TO_TIMESTAMP(%d), TO_TIMESTAMP(%d), '%s',   '%s',   '%s', %f,      %f,      %f,        %f,        %f,        %f,          '%s', '%s',   '%s',        %f,       %d    );"
+	return fmt.Sprintf(insquery+fields+valuesfmt, d.dataTable, // table to insert to
+		d.Network,                      // nwtype
+		d.ChainID,                      // network
+		item.Time,                      // time
+		currentTime,                    // inserted_at
+		item.Token0.String()[2:],       // token0 (removed 0x prefix)
+		item.Token1.String()[2:],       // token1 (removed 0x prefix)
+		item.PairContract.String()[2:], // pair
+		item.Amount0,                   // amount0
+		item.Amount1,                   // amount1
+		0.0,                            // amountusd, FIXME
+		item.Reserve0,                  // reserves0
+		item.Reserve1,                  // reserves1
+		0.0,                            // reservesusd, FIXME
+		"burn",                         // type
+		"",                             // sender FIXME
+		item.Transaction.String()[2:],  // transaction (removed 0x prefix)
+		0.0,                            // slippage
+		item.Height,                    // height
+	)
+}
+
+func (d *DBConn) getQueryStringBlockSynopsis(blockHeight uint64, currentTime int64, bm itypes.BlockSynopsis) string {
 	const insquery string = "INSERT INTO %s "
 	const fields string = "(nwtype, network, height, inserted_at, mint_logs, burn_logs, total_logs) "
 	const valuesfmt string = "VALUES ('%s', %d, %d,  TO_TIMESTAMP(%d), %d,   %d,        %d);"
-	query := fmt.Sprintf(insquery+fields+valuesfmt, d.metaTable, // table to insert to
+	return fmt.Sprintf(insquery+fields+valuesfmt, d.metaTable, // table to insert to
 		d.Network,    // nwtype
 		d.ChainID,    // network
-		block,        // height
+		blockHeight,  // height
 		currentTime,  // inserted_at
 		bm.MintLogs,  // mint_logs
 		bm.BurnLogs,  // burn_logs
 		bm.TotalLogs, // total_logs
 	)
-	// log.Info(query)
-	_, err := dbTx.ExecContext(*dbCtx, query)
-	util.ENOK(err)
 }
