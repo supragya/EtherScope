771fc0143fd1d7ebe330a7f153de05dd0c794f28
Supragya Raj <supragyaraj@gmail.com>
2022-07-26 11:12:31 +0530

    removed: dependency on realtime logger

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/Blockpour/Blockpour-Geth-Indexer
 go 1.18
 
 require (
-	github.com/banzaicloud/logrus-runtime-formatter v0.0.0-20190729070250-5ae5475bae5e
 	github.com/ethereum/go-ethereum v1.10.20
 	github.com/hashicorp/golang-lru v0.5.5-0.20210104140557-80c98217689d
 	github.com/lib/pq v1.10.6
--- logger/logger.go
+++ logger/logger.go
@@ -1,15 +1,68 @@
 package logger
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
 
-	runtime "github.com/banzaicloud/logrus-runtime-formatter"
+	// runtime "github.com/banzaicloud/logrus-runtime-formatter"
+	"github.com/sirupsen/logrus"
 	log "github.com/sirupsen/logrus"
 )
 
+const (
+	logrusStackJump          = 4
+	logrusFieldlessStackJump = 6
+)
+
+// Formatter decorates log entries with function name and package name (optional) and line number (optional)
+type RuntimeFormatter struct {
+	ChildFormatter logrus.Formatter
+	// When true, line number will be tagged to fields as well
+	Line bool
+	// When true, package name will be tagged to fields as well
+	Package bool
+	// When true, file name will be tagged to fields as well
+	File bool
+	// When true, only base name of the file will be tagged to fields
+	BaseNameOnly bool
+}
+
+// Format the current log entry by adding the function name and line number of the caller.
+func (f *RuntimeFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+	function, file, line := f.getCurrentPosition(entry)
+
+	packageEnd := strings.LastIndex(function, ".")
+	functionName := function[packageEnd+1:]
+
+	entry.Data = logrus.Fields{"CODE": fmt.Sprintf("(%s) %s:%s", functionName, filepath.Base(file), line)}
+	return f.ChildFormatter.Format(entry)
+}
+
+func (f *RuntimeFormatter) getCurrentPosition(entry *logrus.Entry) (string, string, string) {
+	skip := logrusStackJump
+	if len(entry.Data) == 0 {
+		skip = logrusFieldlessStackJump
+	}
+start:
+	pc, file, line, _ := runtime.Caller(skip)
+	lineNumber := ""
+	if f.Line {
+		lineNumber = fmt.Sprintf("%d", line)
+	}
+	function := runtime.FuncForPC(pc).Name()
+	if strings.LastIndex(function, "sirupsen/logrus.") != -1 {
+		skip++
+		goto start
+	}
+	return function, file, lineNumber
+}
+
 // Infallible
 func SetupLog() {
-	formatter := runtime.Formatter{ChildFormatter: &log.TextFormatter{
+	formatter := RuntimeFormatter{ChildFormatter: &log.TextFormatter{
 		FullTimestamp:   true,
 		TimestampFormat: "06/01/02 15:04:05.00 Z0700",
 	},
