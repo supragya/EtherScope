69c8725f8c0ed0007e1396afba2ccac1d4a676f2
Supragya Raj <supragyaraj@gmail.com>
2022-08-18 21:59:14 +0530

    test: Uniswap V3 decrease liquidity

--- indexer/realtimeUniswapV3_test.go
+++ indexer/realtimeUniswapV3_test.go
@@ -36,3 +36,17 @@ func TestUniswapV3IncreaseLiquidity(t *testing.T) {
 	assert.Equal(t, 1, len(items), "one mint not found")
 	assert.Equal(t, itypes.BlockSynopsis{MintLogs: 1, TotalLogs: 1}, bm, "one mint not found")
 }
+
+func TestUniswapV3DecreaseLiquidity(t *testing.T) {
+	var (
+		_log  = loadLog(t, "../test/uniswapV3DecreaseLiquidityExample.json")
+		ri    = NewRealtimeIndexer(0, []string{"https://rpc.ankr.com/eth"}, &db.DBConn{ChainID: 1}, []string{})
+		bm    = itypes.BlockSynopsis{}
+		mt    = sync.Mutex{}
+		items []interface{}
+	)
+
+	ri.processBurnV3(_log, &items, &bm, &mt)
+	assert.Equal(t, 1, len(items), "one burn not found")
+	assert.Equal(t, itypes.BlockSynopsis{BurnLogs: 1, TotalLogs: 1}, bm, "one burn not found")
+}
